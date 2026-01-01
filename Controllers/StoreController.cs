e2ad050d3c063683d9bbf59c32ecd7bcb10fe54c
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-01 21:34:48 +0100

    done

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+ 
+namespace _4_aspnetcore_controllers.Controllers;
+
+public class StoreController : Controller
+{
+    [Route("store/books/{id}")]
+    public IActionResult GetBooks()
+    {
+        int id = Convert.ToInt32(Request.RouteValues["id"]);
+        return Content($"List of books from StoreController, {id}", "text/plain");
+
+    }
+}
