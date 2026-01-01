44c2dea3eabd7af680b3e1c17e9e618db035fc96
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-01 16:37:31 +0100

    start

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _4_aspnetcore_controllers.Controllers;
+
+[Controller]
+public class HomeController : ControllerBase  //:Controller invece lo usi se usi le View, ma per apis non ti servono
+{
+    [Route("hello1")]
+    [Route("hello2")]
+    [Route("/")]
+    public string method1()
+    {
+        return "Hello from HomeController.method1";
+    }
+
+    [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
+    public string contactUs()
+    {
+        return "Hello from HomeController.method1";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+namespace _4_aspnetcore_controllers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers(); //aggiungi i controller
+            var app = builder.Build();
+            app.MapControllers(); //mappa i controller
+            app.Run();
+        }
+    }
+}
